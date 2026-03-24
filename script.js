4f8e12d83ff2d53c1261a04609a6743e6a08cf32
Not-Yet-Nixon <Rob.hitotsume@gmail.com>
2026-03-23 20:22:54 -0500

    feat: add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("hello world!!!");
+
+function getComputerChoice () {
+    let x = Math.floor(Math.random() * 3) +1;
+    console.log(x);
+    return x;
+}
+
+
+let compChoice = getComputerChoice();
+
+console.log(compChoice);
+
+if (compChoice === 1) {
+    console.log("Rock");
+} else if (compChoice === 2) {
+    console.log("Paper");
+} else {
+    console.log("Scissors");
+}
