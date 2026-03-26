7e35d43f54543bd2b4c490652da8e71ed9458a08
Not-Yet-Nixon <Rob.hitotsume@gmail.com>
2026-03-25 19:17:03 -0500

    feat: add game logic for single round

--- script.js
+++ script.js
@@ -1,31 +1,57 @@
-
+// computer choice logic
 function getComputerChoice() {
     let x = Math.floor(Math.random() * 3) +1;
+    if (x === 1) return "rock";
+    if (x === 2) return "paper";
+    if (x === 3) return "scissors";
     return x;
 }
 
+let computerChoice = getComputerChoice();
 
-let compChoice = getComputerChoice();
-
-console.log(compChoice);
-
-if (compChoice === 1) {
-    console.log("Rock");
-} else if (compChoice === 2) {
-    console.log("Paper");
-} else {
-    console.log("Scissors");
-}
-
-
+// Player choice logic
 function getHumanChoice() {
     let humanChoice;
     humanChoice = window.prompt("Rock, Paper, or Scissors?");
-    console.log(humanChoice)
-    return humanChoice
+    return humanChoice.trim().toLocaleLowerCase();
 
 }
 
-getHumanChoice();
+// Game round logic
+let playerScore = 0;
+let computerScore = 0;
 
 
+function playRound(humanChoice, computerChoice) {
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+
+    console.log("computer threw " + computerChoice);
+    console.log("player threw " + humanChoice);
+
+    if (humanChoice === computerChoice) {
+        console.log("its a tie");
+    } else if (humanChoice === "rock")
+        { if (computerChoice === "paper") {
+            console.log("player lost, paper beats rock")
+        } else {
+            console.log("Player won! Rock beats Scissors")
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "scissors"){
+            console.log("Player lost. Scissors beats Paper.")
+        } else if (computerChoice === "rock") {
+            console.log("Player wins!, Paper beats Rock!")
+        }
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "rock"){
+            console.log("Player lost. rock beats scissors.")
+        } else if (computerChoice === "paper") {
+            console.log("Player wins!, scissors beats paper!")
+        }
+    }
+
+
+}
+
+playRound();
